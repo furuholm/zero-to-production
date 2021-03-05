d62f78b5134ee66f91e9d0898697f0263c1e4a7d
Tobias Furuholm <tobiasfuruholm@gmail.com>
2021-03-05 12:17:48 +0100

    Add subscribers to database

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,34 @@
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use log::error;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct SubscriberData {
-    _email: String,
-    _name: String,
+    email: String,
+    name: String,
 }
 
-pub async fn subscribe(_form: web::Form<SubscriberData>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn subscribe(
+    form: web::Form<SubscriberData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, HttpResponse> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    .map_err(|e| {
+        error!("Failed to execute query: {}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
+    Ok(HttpResponse::Ok().finish())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,33 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgConnection, PgPool};
 use zero2prod::configuration::get_configuration;
 
-fn spawn_app() -> String {
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").unwrap();
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::startup::run(listener).unwrap();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .unwrap();
+    let server = zero2prod::startup::run(listener, connection_pool.clone()).unwrap();
     tokio::spawn(async move { server.await });
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
 }
 
 #[actix_rt::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let TestApp { address, db_pool } = spawn_app().await;
 
     let client = reqwest::Client::new();
 
@@ -29,7 +43,7 @@ async fn health_check_works() {
 
 #[actix_rt::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let address = spawn_app();
+    let TestApp { address, db_pool } = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
@@ -57,19 +71,13 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 #[actix_rt::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
-    let app_address = spawn_app();
-    let configuration = get_configuration().unwrap();
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to postgres");
-
+    let TestApp { address, db_pool } = spawn_app().await;
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     // Act
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -80,7 +88,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&mut connection)
+        .fetch_one(&db_pool)
         .await
         .unwrap();
 
